29252dedb80afc924fb2ee9d734180cb5a4ca37a
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2024-10-17 13:58:58 -0400

    youPromised

---
+++ youpromised/src/main/java/com/saptarshi/youpromised/YoupromisedApplication.java
@@ -0,0 +1,13 @@
+package com.saptarshi.youpromised;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoupromisedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoupromisedApplication.class, args);
+	}
+
+}
---
+++ youpromised/src/test/java/com/saptarshi/youpromised/YoupromisedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saptarshi.youpromised;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoupromisedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
