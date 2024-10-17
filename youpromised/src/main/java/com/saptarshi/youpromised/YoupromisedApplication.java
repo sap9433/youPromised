e05fba90ace41aa63a3c280ac18da8b5f884dc7c
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2024-10-17 15:34:12 -0400

    controllers

--- youpromised/src/main/java/com/saptarshi/youpromised/YoupromisedApplication.java
+++ youpromised/src/main/java/com/saptarshi/youpromised/YoupromisedApplication.java
@@ -2,12 +2,26 @@ package com.saptarshi.youpromised;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequestMapping("/")
+@RestController	
 public class YoupromisedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YoupromisedApplication.class, args);
+	}
+	@GetMapping 
+	String 	wola() {
+		return "Hello World6";
+	}
+
+	@GetMapping(path = "/promise")
+	public String 	greeting() {
+		return "Hello Love";
 	}
 
 }
